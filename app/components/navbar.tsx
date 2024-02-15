dcc0873157b519ea7d95e4fff1bbf69370e90fee
Anton Kiiski <antonoliverkiiski@gmail.com>
2024-02-15 11:49:48 +0200

    Make navbar responsive

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -3,19 +3,31 @@ import "../../node_modules/flag-icons/css/flag-icons.min.css";
 
 const Navbar = () => {
   return (
-    <nav className="p-3 bg-blue-500 flex flex-nowrap justify-between">
-      <Image
-        src="/pictures/HFMlogo.png"
-        width={500}
-        height={500}
-        alt="Helsingin Miekkailijat ry logo"
-      />
+    <nav className="p-3 bg-blue-500 flex flex-row justify-between">
+      <div className="lg:hidden w-1/2 md:w-1/4">
+        <Image
+          className="w-full"
+          src="/pictures/HFMlogoonly.png"
+          width={500}
+          height={500}
+          alt="Helsingin Miekkailijat ry logo"
+        />
+      </div>
+      <div className="max-lg:hidden w-1/2">
+        <Image
+          className="w-full"
+          src="/pictures/HFMlogowhite.png"
+          width={500}
+          height={500}
+          alt="Helsingin Miekkailijat ry logo"
+        />
+      </div>
       <div className=" flex flex-row justify-between my-auto">
-        <div className="my-auto pr-5">
-          <button className="bg-white hover:bg-blue-700 font-bold py-2 px-4 border-2 border-white rounded-full text-blue-500 m-2">
+        <div className="my-auto pr-5 flex flex-col md:flex-row text-xs">
+          <button className="bg-white hover:bg-blue-700 text-sm font-bold py-2 px-4 border-2 w-full md:w-36 border-white rounded-full text-blue-500 m-1">
             Empty table
           </button>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border-2 border-white rounded-full">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white text-sm font-bold py-2 px-4 border-2 w-full md:w-36 border-white rounded-full m-1">
             Log out
           </button>
         </div>
