8df7daec5f5299a47e6e5919c7ca8a26f5b76da9
Anton Kiiski <antonoliverkiiski@gmail.com>
2024-02-19 15:13:05 +0200

    Make table responsive

--- app/components/leaderboard.tsx
+++ app/components/leaderboard.tsx
@@ -1,11 +1,11 @@
 const Leaderboard = () => {
   return (
-    <div>
-      <h1>Leaderboard</h1>
-      <div className="overflow-auto max-h-[500px] border-2 border-slate-500">
-        <table className="table-auto w-full">
+    <div className="">
+      <h1 className="py-3">Leaderboard</h1>
+      <div className="overflow-auto max-h-[500px] border-2 rounded-md shadow-md border-slate-500 text-xs md:text-base">
+        <table className="w-full">
           <thead>
-            <tr className="text-white *:py-4 *:sticky *:top-0 *:bg-blue-500 z-10">
+            <tr className="text-white *:py-2 *:md:py-4 *:bg-slate-500 *:w-20">
               <th>Nimi</th>
               <th
                 title="Voitot"
@@ -47,7 +47,7 @@ const Leaderboard = () => {
           </thead>
           <tbody>
             {/* Hard data until logic implemented */}
-            <tr className="*:ring-1 *:p-4 *:text-center *:ring-slate-500">
+            <tr className="*:ring-1 *:py-2 *:md:p-4 *:text-center *:ring-slate-500">
               <td>player1</td>
               <td>1</td>
               <td>100%</td>
@@ -56,7 +56,7 @@ const Leaderboard = () => {
               <td>10</td>
               <td>1</td>
             </tr>
-            <tr className="*:ring-1 *:p-4 *:text-center *:ring-slate-500">
+            <tr className="*:ring-1 *:py-2 *:md:p-4 *:text-center *:ring-slate-500">
               <td>player2</td>
               <td>0</td>
               <td>0%</td>
@@ -65,7 +65,7 @@ const Leaderboard = () => {
               <td>-10</td>
               <td>2</td>
             </tr>
-            <tr className="*:ring-1 *:p-4 *:text-center *:ring-slate-500">
+            <tr className="*:ring-1 *:py-2 *:md:p-4 *:text-center *:ring-slate-500">
               <td>player3</td>
               <td>0</td>
               <td>0%</td>
