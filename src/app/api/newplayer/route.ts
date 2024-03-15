d058fee9cad781092c9216692e83fb20b8757064
Anton <112481379+Kiiskii@users.noreply.github.com>
2024-03-15 14:59:18 +0200

    Newplayerback (#83)
    * Add route for newPlayer, add functionality to add players to database
    
    * Add error popup to notify user that the chosen name is taken
    
    * Delete unnecessary comment
    
    * Add "newplayer" component for adding new players to "player" table.
    
    * Fix "addplayer" to add an existing player to the "tournament_players" table
    
    * Add newplayer translations
    
    * Creating new player also adds them to tournament_players
    
    * Merge main to newplayerback and add functionality to add player to tournament_players when creating new player.

--- src/app/api/newplayer/route.ts
+++ src/app/api/newplayer/route.ts
@@ -1,4 +1,5 @@
 import { db } from "../../../database/database";
+import { getTournamentToday, createTournamentToday } from "@/database/addMatch";
 
 export async function POST(request: Request) {
   const formData = await request.formData();
@@ -6,12 +7,34 @@ export async function POST(request: Request) {
   if (name === null) {
     return Response.json("Name cannot be null");
   }
+  const currentDate = new Date();
+  let tournament = await getTournamentToday(currentDate);
+
+  // create new tournament if no tournaments today
+  if (!tournament) {
+    tournament = await createTournamentToday(currentDate);
+  }
 
   try {
     const result = await db
       .insertInto("players")
       .values({
         player_name: name.toString(),
+      })
+      .executeTakeFirst();
+    console.log(result);
+  } catch (error) {
+    return new Response("Error adding player", { status: 400 });
+  }
+
+  try {
+    const result = await db
+      .insertInto("tournament_players")
+      .values({
+        player_name: name.toString(),
+        tournament_id: tournament.id,
+        hits_given: 0,
+        hits_received: 0,
       })
       .executeTakeFirst();
     console.log(result);
