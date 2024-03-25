1fe6d041ecfa4e8dda6efc32ba6943ad9e535230
Mico <9155442+Miconen@users.noreply.github.com>
2024-03-25 12:11:26 +0200

    Make locales globally available to client components (#112)
    Before this commit the i18n localizations were only wrapped around the home page component, now they should be available globally to all client components.

--- src/app/[locale]/home/page.tsx
+++ src/app/[locale]/home/page.tsx
@@ -2,21 +2,13 @@ import Navbar from "@/components/navbar";
 import ResultsTable from "@/components/Results/ResultsTable";
 import LeaderboardHome from "@/components/leaderboardHome";
 import HomePageButtons from "@/components/homePageButtons";
-import { NextIntlClientProvider } from "next-intl";
-import { getMessages } from "next-intl/server";
 import { TournamentContextProvider } from "@/context/TournamentContext";
 
 const Page = async () => {
-  const messages = await getMessages();
 
   return (
     <>
       <Navbar />
-      {/*
-      wrap client components in NextIntlClientProvider so we can use
-      useTranslations hook in client components
-      */}
-      <NextIntlClientProvider messages={messages}>
         {/*Style later when everything figured out, desktop view looks bad  */}
         <TournamentContextProvider>
           <HomePageButtons />
@@ -25,7 +17,6 @@ const Page = async () => {
             <LeaderboardHome />
           </section>
         </TournamentContextProvider>
-      </NextIntlClientProvider>
     </>
   );
 };
