ba55a2769ce2a3cfde409b4fb06d2fbfa1372d9c
Anton <112481379+Kiiskii@users.noreply.github.com>
2024-05-06 15:03:12 +0300

    Remove test input from brackets (#208)

--- src/components/Results/Brackets/Tournament.tsx
+++ src/components/Results/Brackets/Tournament.tsx
@@ -252,10 +252,6 @@ export default function Tournament() {
     <>
       <div className="container mx-auto sm:my-2 items-center text-xl sm:text-4xl font-bold flex justify-between">
         <TournamentTitle />
-        <div className="flex">
-          <label htmlFor="number-input" className="mb-2 text-sm font-medium text-gray-900">Tournament Capacity</label>
-          <input type="number" id="number-input" onChange={(e) => setCapacity(Number(e.currentTarget.value))} aria-describedby="helper-text-explanation" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="TEST INPUT" required />
-        </div>
       </div>
       <div className="flex mr-3 mt-16 gap-16 mb-10 overflow-x-auto">
         <Rounds tournament={tournament} />
